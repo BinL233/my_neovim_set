f5bdd750de31410ede67a58dbd00d8f5cf7f72ab
binl233 <835741923@qq.com>
2026-02-04 17:00:26 -0500

    Add macOS setup env

---
+++ setup_macos.sh
@@ -0,0 +1,14 @@
+# Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# wget
+brew install wget
+
+# neovim
+brew install neovim
+
+# Oh-my-zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+# zsh auto completion
+brew install zsh-autocomplete
